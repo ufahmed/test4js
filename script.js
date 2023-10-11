8a48ca635f1913f26a1f697c9c6dc48dc41383a8
ufahmed <ahmedsellami375@gmail.com>
2023-10-11 19:25:25 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+count=1;
+function add(){
+ x=document.querySelector("#mot1");
+ count++;
+ x.innerText=count+" like(s)";
+
+}
+count=1;
+function add2(){
+ n=document.querySelector("#mot2");
+ count++;
+ n.innerText=count+" like(s)";
+ 
+}
+count=1;
+function add3(){
+ y=document.querySelector("#mot3");
+ count++;
+ y.innerText=count+" like(s)";
+ 
+}
